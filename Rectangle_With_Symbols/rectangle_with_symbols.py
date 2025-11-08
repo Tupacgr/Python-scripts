0959808725ee7a81e1e38dd7dd52369087479512
Fronx <johnlar0701@gmail.com>
2025-11-08 23:48:44 +0200

    Created a Rectangle with symbols

---
+++ Rectangle_With_Symbols/rectangle_with_symbols.py
@@ -0,0 +1,8 @@
+row = int(input("Enter raws: ")) 
+column = int(input("Enter the column: ")) 
+con = input("Enter the content: ") 
+
+for r in range(row): 
+    for y in range(column): 
+        print(con, end = " ") 
+    print()
