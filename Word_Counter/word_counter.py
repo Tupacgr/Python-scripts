958550b5bb58eb6eb79d136654b30a302695cad4
Fronx <johnlar0701@gmail.com>
2025-11-18 19:39:48 +0200

    v1.2

--- Word_Counter/word_counter.py
+++ Word_Counter/word_counter.py
@@ -1,18 +1,16 @@
 x = "I love python! 1!!Hello My name is John"
-l1 = []
-eid_xar = "0123456789!@#$%^&*()_+-=[]{}|;:'\",.<>?/\\`~"
-
-for i in eid_xar:
-    if i in x:
-        x = x.replace(i, "")
-
-x = x.split()
+y = ""
 
 for i in x:
-    if i.isalpha():
-        l1.append(i)
+    if "a" <= i.lower() <= "z":
+        y += i
+    else:
+        y += " "
 
-length = len(l1)
+x = y
+x = x.split()
+length = len(x)
+
 if length == 1:
     print("The text contains 1 word.")
 else:
